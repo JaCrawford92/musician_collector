3ac547793522ce7905a4c5d2de7e080576cb6d83
JaCrawford92 <cckenny9@gmail.com>
2024-04-27 18:33:34 -0500

    index updated

--- musiciancollector_app/urls.py
+++ musiciancollector_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.home, name='home'),
+    path('musicians/', views.musicians_index, name='index'),
 ]
--- musiciancollector_app/views.py
+++ musiciancollector_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+musicians = [
+    {"name": "John Lennon", "instrument": "Guitar", "genre": "Rock", "band": "The Beatles"},
+    {"name": "Miles Davis", "instrument": "Trumpet", "genre": "Jazz", "solo_artist": True},
+    {"name": "Taylor Swift", "instrument": "Guitar", "genre": "Pop", "solo_artist": True},
+    {"name": "Freddie Mercury", "instrument": "Piano", "genre": "Rock", "band": "Queen"},
+    {"name": "Beyoncé", "instrument": "Vocals", "genre": "R&B", "solo_artist": True},
+    {"name": "Yo-Yo Ma", "instrument": "Cello", "genre": "Classical", "solo_artist": True},
+    {"name": "Kurt Cobain", "instrument": "Guitar", "genre": "Grunge", "band": "Nirvana"},
+    {"name": "Duke Ellington", "instrument": "Piano", "genre": "Jazz", "band": "Duke Ellington Orchestra"}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'musicians/home.html')
 
 def about(request):
     return render(request, 'musicians/about.html')
+
+def musicians_index(request):
+    return render(request, 'musicians/index.html', {
+        'musicians': musicians
+    })
