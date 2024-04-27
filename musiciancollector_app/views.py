fbc964562b37372e30cd8c5af1867e4283898902
JaCrawford92 <cckenny9@gmail.com>
2024-04-27 18:20:46 -0500

    templates added

--- musiciancollector_app/views.py
+++ musiciancollector_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, musiciancollector!')
+    return render(request, 'musicians/home.html')
+
+def about(request):
+    return render(request, 'musicians/about.html')
